0f53e9fa2df51b13e0d61937c318840fb7195926
misazhu <misa.zhu@gmail.com>
2026-02-24 05:52:28 +0800

    mario_lex for compiler

--- mario.h
+++ mario.h
@@ -106,28 +106,6 @@ float       mstr_to_float(const char* str);
 void        mstr_split(const char* str, char c, m_array_t* array);
 int         mstr_to(const char* str, char c, mstr_t* res, bool skipspace);
 
-
-/**======utf8 string functions =======*/
-
-typedef struct st_utf8_reader {
-	const char*         str;
-	uint32_t            offset;
-} utf8_reader_t;
-
-typedef m_array_t utf8_t;
-
-void        utf8_reader_init(utf8_reader_t* reader, const char* s, uint32_t offset);
-bool        utf8_read(utf8_reader_t* reader, mstr_t* dst);
-
-utf8_t*     utf8_new(const char* s);
-void        utf8_free(utf8_t* utf8);
-void        utf8_append_raw(utf8_t* utf8, const char* s);
-void        utf8_append(utf8_t* utf8, const char* s);
-uint32_t    utf8_len(utf8_t* utf8);
-mstr_t*     utf8_at(utf8_t* utf8, uint32_t at);
-void        utf8_set(utf8_t* utf8, uint32_t at, const char* s);
-void        utf8_to_str(utf8_t* utf8, mstr_t* str);
-
 /**====== MARIO_BC ======*/
 
 //bytecode
